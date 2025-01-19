8acc0f89dcd0bdec716c1df8d9e3e416dd9548f1
Matyas Vezer <vezer.m@gmail.com>
2025-01-19 22:59:41 +0100

    ghostty: start tmux automatically

---
+++ ghostty/.config/ghostty/launch-tmux.sh
@@ -0,0 +1,18 @@
+# !/bin/bash
+
+SESSION_NAME="$(~/.config/ghostty/random-session)"
+tmux new-session -s $SESSION_NAME -d
+tmux attach-session -t $SESSION_NAME
+# SESSION_NAME="default"
+#
+# # Check if the session already exists
+# tmux has-session -t $SESSION_NAME 2>/dev/null
+#
+# if [ $? -eq 0 ]; then
+#  # If the session exists, reattach to it
+#  tmux attach-session -t $SESSION_NAME
+# else
+#  # If the session doesn't exist, start a new one
+#  tmux new-session -s $SESSION_NAME -d
+#  tmux attach-session -t $SESSION_NAME
+# fi
