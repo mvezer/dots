b1e063955c21875fe439ae9e557391973c11fd53
Matyas Vezer <vezer.m@gmail.com>
2025-01-24 16:53:37 +0100

    nvim: options fix

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -56,23 +56,6 @@ vim.g.loaded_netrwPlugin = 1
 
 OS = vim.loop.os_uname().sysname
 
--- vim.g.terminal_color_0 = "#2e3436"
--- vim.g.terminal_color_1 = "#cc0000"
--- vim.g.terminal_color_2 = "#4e9a06"
--- vim.g.terminal_color_3 = "#c4a000"
--- vim.g.terminal_color_4 = "#3465a4"
--- vim.g.terminal_color_5 = "#75507b"
--- vim.g.terminal_color_6 = "#0b939b"
--- vim.g.terminal_color_7 = "#d3d7cf"
--- vim.g.terminal_color_8 = "#555753"
--- vim.g.terminal_color_9 = "#ef2929"
--- vim.g.terminal_color_10 = "#8ae234"
--- vim.g.terminal_color_11 = "#fce94f"
--- vim.g.terminal_color_12 = "#729fcf"
--- vim.g.terminal_color_13 = "#ad7fa8"
--- vim.g.terminal_color_14 = "#00f5e9"
--- vim.g.terminal_color_15 = "#eeeeec"
-
 vim.g.sql_type_default = "pgsql"
-DISABLED_PLUGINS = { "bufferline", "yazi", "barbar", "blamer", "zenmode", "noice", "indent-blankline", "nvim-recorder", "lsp" }
 NOTES_DIR = os.getenv("NOTES_DIR") or os.getenv("HOME") .. "/notes"
+
