a43351dafff4d416ff4cdf49e1e135ead3994a8c
Matyas Vezer <matyas.vezer@bauerxcel.de>
2025-02-03 23:47:34 +0100

    hammerspoon: add config for alacritty and qutebrowser

---
+++ hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,7 @@
+hs.hotkey.bind({"cmd"}, "b", function()
+  hs.application.launchOrFocus("qutebrowser")
+end)
+
+hs.hotkey.bind({"cmd"}, "return", function()
+  hs.application.launchOrFocus("alacritty")
+end)
