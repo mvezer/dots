7abcd1b5a458f67cb3c1c08035598bcf23530b52
Matyas Vezer <vezer.m@gmail.com>
2025-02-18 14:49:15 +0100

    nvim: markdown preview opens in a new browser window

--- nvim/.config/nvim/lua/config/markdown-preview.lua
+++ nvim/.config/nvim/lua/config/markdown-preview.lua
@@ -1,4 +1,10 @@
 return function ()
+  vim.api.nvim_exec([[
+  function OpenMarkdownPreview (url)
+    execute "silent ! open -a 'Zen Browser' -n --args --new-window " . a:url
+  endfunction
+  ]], false)
+  vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
   vim.g.mkdp_filetypes = { "markdown" }
   vim.g.mkdp_auto_close = 0
   vim.g.mkdp_theme = "light"
