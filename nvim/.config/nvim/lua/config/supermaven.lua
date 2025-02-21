fb377f1b718a8c922f38ac6d67170d91fb93b5b7
Matyas Vezer <vezer.m@gmail.com>
2025-02-21 19:40:07 +0100

    nvim: rice supermaven (just the suggestion color and key)

--- nvim/.config/nvim/lua/config/supermaven.lua
+++ nvim/.config/nvim/lua/config/supermaven.lua
@@ -1,10 +1,13 @@
+-- https://github.com/supermaven-inc/supermaven-nvim
 return function ()
   require("supermaven-nvim").setup({
-    -- keymaps = {
-    -- }
+    keymaps = {
+      accept_suggestion = "<S-Tab>"
+    },
     color = {
       -- suggestion_color = "#94E2D5", -- catppuccin's cyan
-      suggestion_color = "#40d8e3", -- some cyan that's NOT catppuccin - on purpose
+      -- suggestion_color = "#40d8e3", -- some cyan that's NOT catppuccin - on purpose
+      suggestion_color = "#BAC2DE", -- catppuccin's white
       cterm = 99, -- arbitrary number, the above color only shows up when cterm is set TO ANYTHING
     },
     condition = function()
