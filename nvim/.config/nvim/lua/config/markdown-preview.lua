280f97e1973899c98583872b806434b909dc1f92
Matyas Vezer <vezer.m@gmail.com>
2025-03-13 09:05:53 +0100

    nvim: use chrome for markdown preview

--- nvim/.config/nvim/lua/config/markdown-preview.lua
+++ nvim/.config/nvim/lua/config/markdown-preview.lua
@@ -1,9 +1,16 @@
 return function ()
-  vim.api.nvim_exec([[
+  -- vim.api.nvim_exec([[
+  -- function OpenMarkdownPreview (url)
+  --   execute "silent ! open -a 'Zen Browser' -n --args --new-window " . a:url
+  -- endfunction
+  -- ]], false)
+  vim.cmd([[do FileType]])
+  vim.cmd([[
   function OpenMarkdownPreview (url)
-    execute "silent ! open -a 'Zen Browser' -n --args --new-window " . a:url
-  endfunction
-  ]], false)
+    let cmd = "open -a 'Google Chrome' -n --args --new-window " . shellescape(a:url)
+    silent call system(cmd)
+    endfunction
+    ]])
   vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
   vim.g.mkdp_filetypes = { "markdown" }
   vim.g.mkdp_auto_close = 0
