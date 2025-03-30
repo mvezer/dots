3a46bd153fb90cb462438572dc3116ea49277918
Matyas Vezer <vezer.m@gmail.com>
2025-03-30 22:21:33 +0200

    nvim: catppuccin colors for terminal

--- nvim/.config/nvim/lua/config/catppuccin.lua
+++ nvim/.config/nvim/lua/config/catppuccin.lua
@@ -1,13 +1,14 @@
-return function ()
-  require("catppuccin").setup({
-    flavour = "mocha",
-    -- transparent_background = true,
-    custom_highlights = function ()
-      return {
-        -- In case of using transparent background, Noice complains  if the NotifyBackground hl group is not set
-        NotifyBackground = { bg = "#000000" }
-      }
-    end
-  })
-  vim.cmd.colorscheme("catppuccin")
+return function()
+	require("catppuccin").setup({
+		flavour = "mocha",
+		-- transparent_background = true,
+		term_colors = true,
+		custom_highlights = function()
+			return {
+				-- In case of using transparent background, Noice complains  if the NotifyBackground hl group is not set
+				NotifyBackground = { bg = "#000000" },
+			}
+		end,
+	})
+	vim.cmd.colorscheme("catppuccin")
 end
