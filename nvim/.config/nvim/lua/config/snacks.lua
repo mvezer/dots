8f354c13247a00f237b8512856f337b4a3a9bf91
Matyas Vezer <vezer.m@gmail.com>
2025-04-05 17:31:25 +0200

    nvim: snacks explorer telescope layout

--- nvim/.config/nvim/lua/config/snacks.lua
+++ nvim/.config/nvim/lua/config/snacks.lua
@@ -1,47 +1,48 @@
-return function ()
-  vim.g.snacks_animate = false
-  require("snacks").setup({
-    bigfile = { enabled = true },
-    bufdelete = { enabled = true },
-    dashboard = { enabled = false },
-    debug = { enabled = true },
-    dim = { enabled = false },
-    git = { enabled = true },
-    indent = {
-      enabled = true,
-      animate = {
-        enabled = false
-      }
-    },
-    input = { enabled = true },
-    lazygit = { enabled = true },
-    -- deactivate the notifier in favor of Falke's noice
-    -- notifier = {
-    --   enabled = true,
-    --   level = vim.log.levels.TRACE,
-    --   timeout = 3000
-    -- },
-    notify = { enabled = true },
-    quickfile = { enabled = true },
-    scroll = { enabled = false },
-    statuscolumn = { enabled = true },
-    -- toggle = {
-    --   enabled = true,
-    --   map = vim.keymap.set, -- keymap.set function to use
-    --   which_key = true, -- integrate with which-key to show enabled/disabled icons and colors
-    --   notify = true,
-    -- },
-    words = { enabled = false },
-    zen = { enabled = true },
-    picker = {
-      enabled = true,
-      hidden = true,
-      sources = {
-        explorer = {
-          focus = "list",
-          auto_close = true
-        }
-      }
-    }
-  })
+return function()
+	vim.g.snacks_animate = false
+	require("snacks").setup({
+		bigfile = { enabled = true },
+		bufdelete = { enabled = true },
+		dashboard = { enabled = false },
+		debug = { enabled = true },
+		dim = { enabled = false },
+		git = { enabled = true },
+		indent = {
+			enabled = true,
+			animate = {
+				enabled = false,
+			},
+		},
+		input = { enabled = true },
+		lazygit = { enabled = true },
+		-- deactivate the notifier in favor of Falke's noice
+		-- notifier = {
+		--   enabled = true,
+		--   level = vim.log.levels.TRACE,
+		--   timeout = 3000
+		-- },
+		notify = { enabled = true },
+		quickfile = { enabled = true },
+		scroll = { enabled = false },
+		statuscolumn = { enabled = true },
+		-- toggle = {
+		--   enabled = true,
+		--   map = vim.keymap.set, -- keymap.set function to use
+		--   which_key = true, -- integrate with which-key to show enabled/disabled icons and colors
+		--   notify = true,
+		-- },
+		words = { enabled = false },
+		zen = { enabled = true },
+		picker = {
+			enabled = true,
+			hidden = true,
+			sources = {
+				explorer = {
+					layout = { preset = "telescope", preview = true },
+					focus = "list",
+					auto_close = true,
+				},
+			},
+		},
+	})
 end
