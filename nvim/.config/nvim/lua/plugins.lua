f18c53497aca9cc033d0640d7dcc0d7a1fe6f81c
Matyas Vezer <vezer.m@gmail.com>
2025-05-10 12:43:25 +0200

    nvim: add linux version of markdown preview function

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -97,13 +97,23 @@ local plugin_spec = {
       vim.g.mkdp_filetypes = { "markdown" }
     end,
     config = function()
+      local os = vim.loop.os_uname().sysname
       vim.cmd([[do FileType]])
-      vim.cmd([[
+      if os == "Linux" then
+        vim.cmd([[
+         function OpenMarkdownPreview (url)
+            let cmd = "flatpak run \"com.brave.Browser\""
+            silent call system(cmd)
+         endfunction
+         ]])
+      else
+        vim.cmd([[
          function OpenMarkdownPreview (url)
             let cmd = "open -n -a \"/Applications/Brave Browser.app\" --args --new-window ". shellescape(a:url)
             silent call system(cmd)
          endfunction
-      ]])
+         ]])
+      end
       -- make it OS-dependent - "open" does not work on linux
       vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
     end,
