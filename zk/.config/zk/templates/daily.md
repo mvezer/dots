7caac38ee5dc23962d87c8a1b81f3629ab77d265
Matyas Vezer <vezer.m@gmail.com>
2025-06-25 11:01:34 +0200

    zk: enhanced tooling for quicknotes

---
+++ zk/.config/zk/templates/daily.md
@@ -0,0 +1,9 @@
+---
+title: {{title}}
+id: {{id}}
+date: {{extra.day}}
+tags: [daily]
+---
+# {{title}}
+
+{{content}}
--- zsh/.zsh_includes/zk.zsh
+++ zsh/.zsh_includes/zk.zsh
@@ -1,4 +1,38 @@
 #!/user/bin/env zsh
 
-export ZK_PERSONAL_DIR="$HOME/Dropbox/notes/personal"
-export ZK_WORK_DIR="$HOME/Dropbox/notes/work"
+if [[ "$HOST" == "bebop" ]]; then # we're home :)
+  export ZK_DIR="$HOME/Dropbox/notes"
+else
+  export ZK_DIR="$HOME/notes"
+fi
+
+alias n="pushd &> /dev/null && cd $ZK_DIR && zk edit quick-note && popd &> /dev/null"
+
+# function day() {
+#   pushd &> /dev/null
+#   cd $ZK_DIR
+#   if [ "$1" = "yesterday" ] || [ "$1" = "y" ]; then
+#     if [[ "$OSTYPE" == "darwin"* ]]; then
+#         day=$(date -v -1d +%Y-%m-%d)
+#       else
+#         day=$(date -d "yesterday" +%Y-%m-%d)
+#     fi
+#   elif [ "$1" = "tomorrow" ] || [ "$1" = "t" ]; then
+#     if [[ "$OSTYPE" == "darwin"* ]]; then
+#         day=$(date -v +1d +%Y-%m-%d)
+#       else
+#         day=$(date -d "tomorrow" +%Y-%m-%d)
+#     fi
+#   else
+#       day=$(date +%Y-%m-%d)
+#   fi
+#
+#   file=$(find . -name "*${day}*" -type f 2>/dev/null | head -1)
+#
+#   if [ -n "$file" ]; then
+#       nvim "$file"
+#   else
+#       zk new --title "daily-note-${day}" --working-dir dailies --template daily.md --extra day="$day"
+#   fi
+#   popd &> /dev/null
+# }
