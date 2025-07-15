ddb6a2161db4b6e24d3bd0103eac3cc63f42226b
Matyas Vezer <vezer.m@gmail.com>
2025-07-15 20:06:46 +0200

    qutebrowser: no switching tab display

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -256,7 +256,7 @@ config.bind('<Ctrl+x>', 'tab-close')
 config.bind('M', 'hint links spawn mpv {hint-url}')
 config.bind('xx', 'config-cycle tabs.show always switching')
 config.bind('<Alt-p>', 'spawn --userscript ~/.config/qutebrowser/userscripts/1pass.js')
-config.bind('<Super+b>', 'config-cycle tabs.show always switching')
+config.bind('<Super+b>', 'config-cycle tabs.show always never')
 
 c.fonts.default_family = ['Hack Nerd Font Mono', 'monospace']
 c.fonts.default_size = '13pt'
