b8289f43061933e171fb5bea3688f07061b85e55
Matyas Vezer <vezer.m@gmail.com>
2025-07-17 07:42:50 +0200

    nvim: limit fzf buffers to show only the last 3 segments of the file

--- nvim/.config/nvim/lua/fzf.lua
+++ nvim/.config/nvim/lua/fzf.lua
@@ -3,11 +3,14 @@ return {
     rg_opts = [[--color=never --hidden --files -g "!.git"]],
     fd_opts = [[--color=never --hidden --type f --type l --exclude .git]],
   },
+  buffers = {
+    fzf_opts = { ["--delimiter"] = "/", ["--with-nth"] = "-3..-1" },
+  },
   grep = {
     hidden = true,
   },
   winopts = {
     width = 1.0,
     height = 1.0,
-  }
+  },
 }
