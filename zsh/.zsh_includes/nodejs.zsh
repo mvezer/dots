d4adc8388c15ea6f3029bb7b35f4f51c7333187b
Matyas Vezer <vezer.m@gmail.com>
2025-07-29 13:24:25 +0200

    zsh+node: fix nvm and npm prefix

--- zsh/.zsh_includes/nodejs.zsh
+++ zsh/.zsh_includes/nodejs.zsh
@@ -1,12 +1,11 @@
 #!/usr/bin/env zsh
 
-# global npm packages
-export NPM_PACKAGES="$HOME/.npm-packages"
-export PATH="$PATH:$NPM_PACKAGES/bin"
-# should set once:
-npm config set prefix "$NPM_PACKAGES"
-
 # nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# global npm packages
+# export NPM_PACKAGES="$NVM_DIR/versions/node/v20.11.1"
+# npm config set prefix "$NPM_PACKAGES"
+# export PATH="$PATH:$NPM_PACKAGES/bin"
