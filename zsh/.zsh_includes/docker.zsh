0fb5086ab2fc679ca9d4635f28450f9fcc24ffb3
Matyas Vezer <vezer.m@gmail.com>
2025-08-04 22:11:47 +0200

    docker: lazydocker alias

--- zsh/.zsh_includes/docker.zsh
+++ zsh/.zsh_includes/docker.zsh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+alias ld=lazydocker
+
 function ecr-login () {
   aws ecr get-login-password | docker login -u AWS --password-stdin "https://$(aws sts get-caller-identity --query 'Account' --output text).dkr.ecr.us-east-1.amazonaws.com"
 }
