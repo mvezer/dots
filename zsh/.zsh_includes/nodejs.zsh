a6b53b9222176cdc99e00f07b691567ec6291205
Matyas Vezer <vezer.m@gmail.com>
2025-08-06 18:45:25 +0200

    node: remove the stupid deno include

--- zsh/.zsh_includes/nodejs.zsh
+++ zsh/.zsh_includes/nodejs.zsh
@@ -4,7 +4,7 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-. "/home/mat/.deno/env"
+# . "/home/mat/.deno/env"
 # global npm packages
 # export NPM_PACKAGES="$NVM_DIR/versions/node/v20.11.1"
 # npm config set prefix "$NPM_PACKAGES"
